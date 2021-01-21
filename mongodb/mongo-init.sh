d53b7b3a3d6d38ef02d39bb3f87ad07b1bc48ca4
Miko Chu <angelomycar@gmail.com>
2021-01-21 13:23:40 +0800

    removed not working mongo auth

--- mongodb/mongo-init.sh
+++
@@ -1,9 +0,0 @@
-mongo -- "$MONGO_INITDB_DATABASE" <<EOF
-    db.createUseR({
-        user: "$MONGO_USERNAME",
-        pwd: "$MONGO_PASSWORD",
-        roles: [
-            { role: 'readWrite', db: "$MONGO_DATABASE" }
-        ]
-    })
-EOF
